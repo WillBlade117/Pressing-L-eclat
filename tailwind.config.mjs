a4bc488047f82c2741298f7d664e5697b1fe66fc
William <sart.william@gmail.com>
2026-01-21 20:42:09 +0100

    perf: achieve 100/100 Lighthouse score (SEO, A11y, Best Practices)

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,8 +4,8 @@ export default {
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: ['Inter', 'sans-serif'],
 				serif: ['"Playfair Display"', 'serif'],
+            	sans: ['"Lato"', 'sans-serif'],
 			},
 		},
 	},
